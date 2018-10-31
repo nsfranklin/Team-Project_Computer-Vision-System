3e7b7e3215479c2dcd84f66368364547cf3d4275
nsfranklin <nickfranklin@rocketmail.com>
2018-10-31 14:47:40 +0000

    Added LoadImageSet Function
    Added a load imageset function. using this experience to learn how C++ functions.

--- Computer Vision/Computer Vision/Computer Vision/Computer Vision.cpp
+++ Computer Vision/Computer Vision/Computer Vision/Computer Vision.cpp
@@ -3,16 +3,29 @@
 #include<iostream>
 using namespace std;
 using namespace cv;
+
+void loadImageSet(Mat* image_set, int Length);
+
 int main()
 {
-	Mat img = imread("lena.jpg");
-	if (img.empty())
+	Mat image_array[1] = {};
+	loadImageSet(image_array, 1);
+	if (image_array[0].empty())
 		std::cout << "failed to open img.jpg" << std::endl;
 	else
 		std::cout << "img.jpg loaded OK" << std::endl;
-	Mat img2 = img;
+	Mat img2 = image_array[0];
 	namedWindow("image", WINDOW_NORMAL);
 	imshow("image", img2);
 	waitKey(0);
 	return 0;
+}
+
+void loadImageSet(Mat* image_set, int Length) {
+	String temp;
+	for (int i = 0; i < Length; i++) {
+		temp = to_string(i) + ".jpg";
+	std::cout << temp << std::endl;
+		image_set[i] = imread(temp);
+	}
 }
