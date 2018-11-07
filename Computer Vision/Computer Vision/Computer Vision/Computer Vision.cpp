eb17d48de83343c7cbb595f325e78c37ea25657f
nsfranklin <nickfranklin@rocketmail.com>
2018-11-07 19:28:02 +0000

    KeyPoint Detection completed
    Added ORB keypoint detection.

--- Computer Vision/Computer Vision/Computer Vision/Computer Vision.cpp
+++ Computer Vision/Computer Vision/Computer Vision/Computer Vision.cpp
@@ -2,50 +2,97 @@
 #include<opencv2/opencv.hpp>
 #include<iostream>
 
+
+
 using namespace std;
 using namespace cv;
 
-void loadImageSet(Mat* image_set, int Length);
-void loadImageSet(Mat* image_set, int Length, char prefix);
+void loadImageSet(vector<Mat> *image_set, int Length);
+void loadImageSet(vector<Mat> *image_set, int Length, char prefix);
+void featureMatching(vector<Mat> image_set, vector<vector<KeyPoint>> *keyPointVec);
 
 int main()
 {
-	Mat image_array[2] = {};
-	loadImageSet(image_array, 2);
-	if (image_array[0].empty())
+	time_t start = time(NULL);
+	vector<Mat> image_array = {};
+	loadImageSet(&image_array, 3);
+	std::cout << "load complete" << std::endl;
+	if (image_array.empty())
 		std::cout << "failed to open img.jpg" << std::endl;
 	else
 		std::cout << "img.jpg loaded OK" << std::endl;
-	Mat img2 = image_array[0];
-	Mat img1 = image_array[1];
+	//Mat img = image_array[0];
+	//Mat img1 = image_array[1];
+	//namedWindow("image", WINDOW_NORMAL);
+	//imshow("image", img);
+	//namedWindow("image2", WINDOW_NORMAL);
+	//imshow("image2", image_array[1]);
+	vector<vector<KeyPoint>> KeyPoints;
+
+	featureMatching(image_array, &KeyPoints);
+
+	std::cout << "feature matching complete" << std::endl;
+
+	Mat sampleImage = image_array[0]; //sample to show keypoints
+
+	std::cout << "sample loaded" << std::endl;
+
+	Mat sampleWithKeyPoints; //output with keypoints
+
+	int flags = DrawMatchesFlags::DEFAULT + DrawMatchesFlags::DRAW_RICH_KEYPOINTS;
+
+	drawKeypoints(sampleImage, KeyPoints[0], sampleWithKeyPoints, Scalar::all(-1), flags);
+
 	namedWindow("image", WINDOW_NORMAL);
-	imshow("image", img2);
-	namedWindow("image2", WINDOW_NORMAL);
-	imshow("image2", image_array[1]);
+	imshow("image", sampleWithKeyPoints);
+
+	std::cout << KeyPoints[0].size() << std::endl;
+
 	waitKey(0);
+
+
+	//cameraCalibration();
+	// undistortPoints();
+	// triangulatePoints();
+
+
+	printf("time elapsed: %d\n", (time(NULL) - start));
+	
+
 	return 0;
 }
 
 
-void loadImageSet(Mat* image_set, int Length, char prefix) {  //used to disingue image sets. relation to the same listing
+void loadImageSet(vector<Mat> *image_set, int Length, char prefix) {  //used to disingue image sets. relation to the same listing
 	String temp;											  //c is intened for chessboard calibration images
+	vector<Mat> temp2;
+	
 	for (int i = 0; i < Length; i++) {
 		temp = prefix + to_string(i) + ".jpg";
 	std::cout << temp << std::endl;
-		image_set[i] = imread(temp);
+		temp2.push_back(imread(temp));
 	}
+
+	*image_set = temp2;
+	return;
 }
 
-void loadImageSet(Mat* image_set, int Length) { 
+void loadImageSet(vector<Mat> *image_set, int Length) { 
 	String temp;
+	vector<Mat> temp2;
+
 	for (int i = 0; i < Length; i++) {
 		temp = to_string(i) + ".jpg";
 		std::cout << temp << std::endl;
-		image_set[i] = imread(temp);
+		temp2.push_back(imread(temp));
+		std::cout << temp2.size() << std::endl;
 	}
+
+	*image_set = temp2;
+
 }
 
-void featureMatching(Mat image_set) {
+void featureMatching(vector<Mat> image_set, vector<vector<KeyPoint>> *keyPointVec) {
 	int nfeature = 500; //max number of feature to keep
 	float scaleFactor = 1.2f; //Effectly this is a measure of precision
 	int edgeThreshold = 20; //Rather clear. How much of the edge should be ignored for feature detection
@@ -54,14 +101,15 @@ void featureMatching(Mat image_set) {
 	int scoreType = cv::ORB::HARRIS_SCORE; //This is used to rank features to determine the best. A faster alternative is FAST_SCORE
 	int patchSize = 31; //This size of patches by BRIEF when descripting and matching keypoints found by FAST
 	int fastThreshold = 20; //default seems to be 20. It is the threshold in intinity different between the pixel at the center and the points round it.
-
-	vector<KeyPoint> keypoints;
+	Mat mask = Mat(); //option part of detect. strange its still a needed parameter
+	vector<vector<KeyPoint>> temp;
 
 	Ptr<ORB> orb = cv::ORB::create(nfeature, scaleFactor, edgeThreshold, firstLevel, WTA_K, scoreType, patchSize, fastThreshold);
 
-	orb->detect(image_set, keypoints);
+	std::cout << "orb created" << std::endl;
 
-
+	orb->detect(image_set, temp, mask);
+	*keyPointVec = temp;
 
 }
 
